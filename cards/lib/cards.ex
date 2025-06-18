31d508bce43a72fdd3c8e57503b0ab3cc4a3d8f5
Hasini Wickramasinghe <hasiniwickramasinghe.hw@gmail.com>
2025-06-18 17:21:35 +0530

    load function

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -32,6 +32,15 @@ defmodule Cards do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "This file is not found"
+    end
+  end
 end
 
 # deck = Cards.create_deck()
