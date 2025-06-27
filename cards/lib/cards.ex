0538a971ea3de1bca70f1c1e0bc8b3533d77e399
Hasini Wickramasinghe <hasiniwickramasinghe.hw@gmail.com>
2025-06-27 18:18:11 +0530

    combine 3 fuctions in one

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -39,4 +39,10 @@ defmodule Cards do
       {:error, _reason} -> "This file is not found"
     end
   end
+
+  def create_hand(hand_size) do
+    deck = Cards.create_deck()
+    deck = Cards.shuffle(deck)
+    hand = Cards.deal(deck, hand_size)
+  end
 end
